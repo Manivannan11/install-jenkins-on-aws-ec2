c407a65a3f8b1b768d6b09128d670f2557d10c07
Manivannan11 <50003274+Manivannan11@users.noreply.github.com>
2020-05-02 21:28:57 +0530

    Update pro2.py
    made changes on 3rd line

--- pro2.py
+++ pro2.py
@@ -1,6 +1,6 @@
 a=int(input('enter a value:'))
 b=int(input('enter b value:'))
-c==int(input('enter c value:'))
+c=int(input('enter c value:'))
 if a>b and a>c :
    print('a is greater')
 elif b>c :
