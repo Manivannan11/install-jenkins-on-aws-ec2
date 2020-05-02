159b4ec89a6364d5e4f6509b67c2659fb1c03268
Manivannan11 <50003274+Manivannan11@users.noreply.github.com>
2020-05-02 21:26:01 +0530

    Update pro2.py
    added : based on the eeror

--- pro2.py
+++ pro2.py
@@ -1,7 +1,7 @@
 a=int(input('enter a value:'))
 b=int(input('enter b value:'))
 c==int(input('enter c value:'))
-if a>b and a>c
+if a>b and a>c :
    print('a is greater')
 elif b>c
    print('b is greater')
