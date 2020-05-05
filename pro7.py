b71f76d5173655eb28a2f4afdd207b481f14311e
Manivannan11 <50003274+Manivannan11@users.noreply.github.com>
2020-05-05 11:19:27 +0530

    Update pro7.py
    savew

--- pro7.py
+++ pro7.py
@@ -1,6 +1,7 @@
 f=open("/home/ec2-user/man/install-jenkins-on-aws-ec2/sample.txt","r+")
 a=[]
 for line in f.readlines():
+   print(line)
    value=line.split()
    a.append([value[0],value[1],value[2]])
 print(a)
