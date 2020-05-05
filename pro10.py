0bd67fd0625c5b5e6bc1ccbba4840d708b5f1211
Manivannan11 <50003274+Manivannan11@users.noreply.github.com>
2020-05-05 17:56:39 +0530

    Create pro10.py
    save

---
+++ pro10.py
@@ -0,0 +1,12 @@
+class Demo():
+  def __init__(self,a,b):
+    self.a=a
+    self.b=b
+    c=a+b
+    print(c)
+  def display():
+    print("value of a is",self.a)
+    print("value if b is",self.b)
+
+obj=Demo(10,20)
+obj.display
