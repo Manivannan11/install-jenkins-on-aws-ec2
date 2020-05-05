4ed93b6213a93230400d10d5c17c56c9a514c254
Manivannan11 <50003274+Manivannan11@users.noreply.github.com>
2020-05-05 11:42:52 +0530

    Update pro8.py
    sav

--- pro8.py
+++ pro8.py
@@ -1,11 +1,8 @@
-import sys
-print(sys.version)
 a=raw_input('enter a keyword:')
 f=open("/home/ec2-user/man/install-jenkins-on-aws-ec2/sample.txt","r+,w+")
 count=0
 for line in f.readlines():
    count+=1
-   print(line)
    if a in line:
       print("keyword found in the line:",count)
 f.close()
